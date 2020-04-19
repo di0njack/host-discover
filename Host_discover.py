8d8d0ac75e67f44eb7fd8b59744a16909b685a08
di0njack <jorge.blanco@teamcmp.com>
2020-04-19 02:43:55 +0200

    Added new IP input file functionality

--- Host_discover.py
+++ Host_discover.py
@@ -6,32 +6,68 @@ __version__     = 'v1.0'
 __last_update__ = 'May 2019'
 
 import socket
-import ipaddress
+import ipaddress,argparse,sys
 
 
 results = []
-host_name = ""
-networks = ["127.0.0.1","192.168.0.1"]
 
+def file_to_list(file_path): #Read a TXT file with an item per line and returns a load Python list containing these items
+   
+    my_list = []
 
-for network in networks:
-    print ("- Processing network: " + network + '\n')
-    for ip in ipaddress.IPv4Network(network):
-        ip_addr = ip._string_from_ip_int(ip._ip)
+    try:
+        with open(file_path, 'r+', encoding="utf-8") as infile:
+                        lines = filter(None, (line.rstrip() for line in infile))
+                        for myline in lines: 
+                                if not myline.startswith('#'): #FILTER OUT FILE COMMENTS
+                                    my_list.append(myline.strip('\n')) #DELETE NEWLINE CHARACTERS
+    except Exception as e:
+        print('[!] Error when loading the file: %s.\n\tDetails: %s\n' % (file_path,str(e)))
+        sys.exit(1)
 
-       
-        #print ("   * IPv4: " +  ip_addr + '\n')
-        
-        
+    return my_list
+
+def write_file(path,data):
+    try:
+        with open(path ,'a') as file:
+            file.write(data)
+    except Exception as e:
+        print('[!] Error when writing data to a file: %s.\n\tDetails: %s\n' % (path,str(e)))
+        sys.exit(1)
+
+def resolve_ip(ips):
+    for ip in ips:
+        print ("- Processing IP: " + ip + '\n')
         try:
-            host_name = socket.gethostbyaddr(ip_addr) #CHECKS IF HOST IS ALIVE
-            print ("      > Found hostname: " + str(host_name) + '\n')
-        except Exception as e:
-            print ("      > Error!: " + str(e) + '\n') 
+            ip_obj = ipaddress.IPv4Address(ip)
+            ip_addr = ip_obj._string_from_ip_int(ip_obj._ip)
+            try:
+                host_name = socket.gethostbyaddr(ip_addr) #CHECKS IF HOST IS ALIVE
+                print ("      > Found hostname: " + str(host_name) + '\n')
+            except Exception as e:
+                print ("      > Error!: " + str(e) + '\n') 
+                continue
+                
+            if "some_keyword" in host_name: #CHOOSE A KEYWORD IDENTIFYING YOUR PENTEST SCOPE FOR FILTERING OUT RELATED HOSTS
+                results.append(host_name)
+        except Exception:
             continue
-            
-        if "some_keyword" in host_name: #CHOOSE A KEYWORD IDENTIFYING YOUR PENTEST SCOPE FOR FILTERING OUT RELATED HOSTS
-            results.append(host_name)
-  
 
-print (results)
+# *** MAIN CODE ***
+if __name__ == '__main__':
+     #ARG PARSER
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', help='Input IP file', dest='input_ip')
+    args = parser.parse_args()
+
+    #SAVE INPUT ARGS
+    input_file = args.input_ip
+
+    #READ FILE
+    ips_list = file_to_list(input_file)
+
+    #RESOLVE IP
+    resolve_ip(ips_list)
+
+    #SAVE RESULTS
+    write_file("results.txt",results)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Reverse DNS resolution on whole AS networks for filtering out related hosts within our pentest scope. Useful for discovery of hosts within large AS networks.
+
+Usage: python3 Host_discover.py -i my_ips.txt
